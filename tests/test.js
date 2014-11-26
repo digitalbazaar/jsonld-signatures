11c22ed2c7f8f320dc175c0b7bafa6450a9ccc86
Manu Sporny <msporny@digitalbazaar.com>
2014-11-26 16:34:38 -0500

    Add test suite stubs.

---
+++ tests/test.js
@@ -0,0 +1,75 @@
+/**
+ * Test runner for JSON-LD Signatures library.
+ *
+ * @author Dave Longley <dlongley@digitalbazaar.com>
+ * @author Manu Sporny <msporny@digitalbazaar.com>
+ *
+ * Copyright (c) 2014 Digital Bazaar, Inc. All rights reserved.
+ */
+(function() {
+
+'use strict';
+
+// detect node.js (vs. phantomJS)
+var _nodejs = (typeof process !== 'undefined' &&
+  process.versions && process.versions.node);
+
+if(_nodejs) {
+  var _jsdir = getEnv().JSDIR || 'js';
+  var fs = require('fs');
+  var path = require('path');
+  var jsonld = require('jsonld');
+  var jsigs = require('../' + _jsdir + '/jsonld-signatures')(jsonld);
+  var assert = require('assert');
+  var program = require('commander');
+  program
+    .option('--bail', 'Bail when a test fails')
+    .parse(process.argv);
+} else {
+  var fs = require('fs');
+  var system = require('system');
+  require('./setImmediate');
+  var _jsdir = getEnv().JSDIR || 'js';
+  require('jsonld');
+  var jsigs = require('../' + _jsdir + '/jsonld-signatures')(jsonld);
+  jsigs = jsigsjs;
+  window.Promise = require('es6-promise').Promise;
+  var assert = require('chai').assert;
+  require('mocha/mocha');
+  require('mocha-phantomjs/lib/mocha-phantomjs/core_extensions');
+  var program = {};
+  for(var i = 0; i < system.args.length; ++i) {
+    var arg = system.args[i];
+    if(arg.indexOf('--') === 0) {
+      var argname = arg.substr(2);
+      switch(argname) {
+      default:
+        program[argname] = true;
+      }
+    }
+  }
+
+  mocha.setup({
+    reporter: 'spec',
+    ui: 'bdd'
+  });
+}
+
+// run tests
+describe('JSON-LD Signatures', function() {
+  it('should successfully verify a local signed document', function(done) {
+    var signedDoc = {};
+    jsigs.verify(signedDoc, function(err, verified) {
+      assert.isTrue(verified, 'signature verification failed');
+      done();
+    });
+  });
+});
+
+if(!_nodejs) {
+  mocha.run(function() {
+    phantom.exit();
+  });
+}
+
+})();
