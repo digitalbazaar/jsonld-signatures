f9bb1093c3877e97dd40736841ffd4ebcd92b552
Dave Longley <dlongley@digitalbazaar.com>
2018-02-08 16:49:14 -0500

    Remove RsaSignature2017 and implement RsaSignature2018.
    - Start transitioning to use of `proof` over `signature` for
      unification with LD Proofs.
    - This library should shift toward an LD-Proof design and rename
      of `ld-proofs`.

--- tests/test.js
+++ tests/test.js
@@ -4,19 +4,17 @@
  * @author Dave Longley
  * @author David I. Lehn
  *
- * Copyright (c) 2011-2017 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2011-2018 Digital Bazaar, Inc. All rights reserved.
  */
 const assert = require('chai').assert;
 const common = require('./test-common');
 const jsonld = require('../node_modules/jsonld');
 const jsigs = require('..');
-const jws = require('../node_modules/jws');
 
 const options = {
   assert: assert,
   jsigs: jsigs,
   jsonld: jsonld,
-  jws: jws,
   nodejs: true
 };
 
